df432cf510e182e40b274c8c49ebea0de32f05e0
Erick Rashón <ergonzalez209@gmail.com>
2022-06-29 14:49:54 -0600

    fix excel generate

--- commons.py
+++ commons.py
@@ -1,9 +1,9 @@
 from xlsxwriter import Workbook
 from datetime import datetime
 from odbc import *
-from querys import *
+from SQLQueries import *
 from io import BytesIO
-
+# from info import data
 
 def send_to_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(datetime.today()).replace(":","")+".xlsx", sheet_name="Especiales_2", headers=None):
 
@@ -93,7 +93,6 @@ def send_to_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(datetime
         elif [name,date] in temp:
             subindex = index-1
             data_processed[subindex][5] = '+'.join((str(data_processed[subindex][5]), str(amount)))
-            #duplicate_name.append([subindex,date,day_of_week,customer,name,str(amount),payment_method])
 
 
     index = 1
@@ -139,7 +138,6 @@ def send_to_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(datetime
 
     return file_on_memory
 
-
 def send_to_simple_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(datetime.today()).replace(":","")+".xlsx", sheet_name="Especiales_2", headers=None):
 
     file_on_memory = BytesIO()
@@ -151,12 +149,11 @@ def send_to_simple_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(d
     wrap_format = workbook.add_format({'text_wrap': 0})
     bold = workbook.add_format({'bold': 1})
     row, col = 1, 0
-    COLUMNAS = {'TOTAL':'C','LUNES':'D', 'MARTES':'E', 'MIÉRCOLES':'F', 'JUEVES': 'G', 'VIERNES' : 'H', 'SÁBADO': 'I'} 
-    # header_name = [column[0].replace("_", " ").upper() for column in data.description]
+
     temp = []
     data_processed=[]
     duplicate_name=[]
-
+    COLUMNAS = {'TOTAL':'C','LUNES':'D', 'MARTES':'E', 'MIÉRCOLES':'F', 'JUEVES': 'G', 'VIERNES' : 'H', 'SÁBADO': 'I'} 
     pos = 0
     index = 0
 
@@ -181,6 +178,7 @@ def send_to_simple_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(d
                 .replace("  PARRILLA 2","")
                 .replace(" CASIT 45 Y 20 AMARILLAS","")
             ).strip()
+
         if [name,date] not in temp:
             temp.append([name, date])
             data_processed.append([index,date,day_of_week,customer,name,str(amount),payment_method])
@@ -190,44 +188,45 @@ def send_to_simple_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(d
             data_processed[subindex][5] = '+'.join((str(data_processed[subindex][5]), str(amount)))
 
 
-    index = 0
+    index = 1
     for rowid, date,day_of_week,customer,name,amount,payment_method in data_processed:
         worksheet.write(index, 0, name)
         if name not in duplicate_name:
             duplicate_name.append(name)
             if day_of_week == 2:
-                worksheet.write(index, 3, '='+amount)
+                worksheet.write(index, 3, '='+amount, number_format)
             elif day_of_week == 3:
-                worksheet.write(index, 4, '='+amount)
+                worksheet.write(index, 4, '='+amount, number_format)
             elif day_of_week == 4:
-                worksheet.write(index, 5, '='+amount)
+                worksheet.write(index, 5, '='+amount, number_format)
             elif day_of_week == 5:
-                worksheet.write(index, 6, '='+amount)
+                worksheet.write(index, 6, '='+amount, number_format)
             elif day_of_week == 6:
-                worksheet.write(index, 7, '='+amount)
+                worksheet.write(index, 7, '='+amount, number_format)
             elif day_of_week == 7:
-                worksheet.write(index, 8, '='+amount)
+                worksheet.write(index, 8, '='+amount, number_format)
+            #worksheet.write(index, 9, customer)
             index+=1
         elif name in duplicate_name:
             if day_of_week == 2:
-                worksheet.write(duplicate_name.index(name)+1, 3, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 3, '='+amount, number_format)
             elif day_of_week == 3:
-                worksheet.write(duplicate_name.index(name)+1, 4, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 4, '='+amount, number_format)
             elif day_of_week == 4:
-                worksheet.write(duplicate_name.index(name)+1, 5, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 5, '='+amount, number_format)
             elif day_of_week == 5:
-                worksheet.write(duplicate_name.index(name)+1, 6, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 6, '='+amount, number_format)
             elif day_of_week == 6:
-                worksheet.write(duplicate_name.index(name)+1, 7, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 7, '='+amount, number_format)
             elif day_of_week == 7:
-                worksheet.write(duplicate_name.index(name)+1, 8, '='+amount)
+                worksheet.write(duplicate_name.index(name)+1, 8, '='+amount, number_format)
+            #worksheet.write(index, 9, customer)
         worksheet.write_formula('C'+str(index), '=SUM('+COLUMNAS['LUNES']+str(index)+':'+COLUMNAS['SÁBADO']+str(index)+')')
 
-    worksheet.set_column("A:A", 45)
-    
     for DIA in COLUMNAS:
+        worksheet.write_formula(COLUMNAS[DIA]+str(index+2), '=SUM('+COLUMNAS[DIA]+str(1)+':'+COLUMNAS[DIA]+str(index)+")")
 
-        worksheet.write_formula(COLUMNAS[DIA]+str(index+2), '=SUM('+COLUMNAS[DIA]+str(1)+':'+COLUMNAS[DIA]+str(index)+")",bold )
+    worksheet.set_column("A:A", 45)
 
     workbook.close()
     file_on_memory.seek(0)
@@ -235,9 +234,7 @@ def send_to_simple_xlsx_fix(data, file_name="Reporte Especiales2 Contado"+ str(d
     return file_on_memory
 
 
-
-def send_to_xlsx(data, file_name="Reporte Especiales2 Contado"+ str(datetime.today()).replace(":","")+".xlsx", sheet_name="Especiales_2", headers=None):
-
+def send_to_xlsx(data, file_name="Reporte Especiales2 Contado " + str(datetime.today()).replace(":","")+".xlsx", sheet_name="Especiales_2", headers=None):
 
     workbook = Workbook(file_name)
     worksheet = workbook.add_worksheet(sheet_name)
@@ -250,18 +247,16 @@ def send_to_xlsx(data, file_name="Reporte Especiales2 Contado"+ str(datetime.tod
 
     # headers = [header[0].replace("_", " ").upper() for header in data.description]
     
-    [header.replace("_", " ").upper() for header in headers]
 
+    [header.replace("_", " ").upper() for header in headers]
     worksheet.set_column(row,5,45)
     
     for header_name in headers:
         worksheet.write(row, col_count, header_name, bold)
-
         col_count += 1
 
     for line in data:
         row += 1
-
         for index in range(len(line)):
             print(line[index], isinstance(line[index], float))
             if isinstance(line[index], float):
@@ -271,16 +266,19 @@ def send_to_xlsx(data, file_name="Reporte Especiales2 Contado"+ str(datetime.tod
 
     workbook.close()
 
-# mysql = Odbc("MYSQL")
-# conn = mysql.connect(auto_commit=False)  # auto_comit defaul is false
-# cursor = conn.cursor()
-# result = cursor.execute(ESPECIALES_2_TO_EXCEL, '2022-06-13', '2022-06-18',0)
-# data = result.fetchall()
-# import pprint 
-# pprint.pprint(data)
+mysql = Odbc("MYSQL")
+conn = mysql.connect(auto_commit=False)  # auto_comit defaul is false
+cursor = conn.cursor()
+result = cursor.execute(ESPECIALES_2_TO_EXCEL, '2022-06-20', '2022-06-25',0)
+data = result.fetchall()
 
 
-# send_to_xlsx_fix(data, headers=['fecha', 'COLUMNAS[DIA]', 'cliente', 'nombre', 'monto', 'forma'])
+file = send_to_simple_xlsx_fix(data, headers=['fecha', 'COLUMNAS[DIA]', 'cliente', 'nombre', 'monto', 'forma'])
+
+with open("excel_data2.xlsx", "w+b") as f:
+    f.write(file.read())
+    f.close()
+
 
 
  # repollo
